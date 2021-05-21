4ee3d097bf7d8f99b7e12306e50d213c7f9f8054
Ahmed Dawoud <adawoud1000@hotmail.com>
2021-05-21 10:34:16 +0200

    Add ability to draw Rectangle

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 math.randomseed(os.time())
-
+crtl = false
+alt = false
 r, g, b, a = math.random(), math.random(), math.random(), 1
 rd = 10
 function love.load()
@@ -11,9 +12,19 @@ function love.load()
     buffers[true] = love.graphics.newCanvas()
     buffers[false] = love.graphics.newCanvas()
     buffers.current = true
+    currentMode = 0
 end
 function love.update()
-    dt = 1 / 100000000000
+    if love.keyboard.isDown('lctrl') or love.keyboard.isDown('rctrl') then
+        crtl = true
+    else
+        crtl = false
+    end
+    if love.keyboard.isDown('ralt') or love.keyboard.isDown('lalt') then
+        alt = true
+    else
+        alt = false
+    end
 end
 function love.draw()
     if love.mouse.isDown(1) then
@@ -23,7 +34,11 @@ function love.draw()
 
         love.graphics.setColor(r, g, b, a)
         -- draw new content over last frame
-        love.graphics.circle('fill', love.mouse.getX(), love.mouse.getY(), rd)
+        if currentMode == 0 then
+            love.graphics.circle('fill', love.mouse.getX(), love.mouse.getY(), rd)
+        elseif currentMode == 1 then
+            love.graphics.rectangle('fill', love.mouse.getX() - rd / 2, love.mouse.getY() - rd / 2, rd, rd)
+        end
         love.graphics.setColor(1, 1, 1, 1)
         love.graphics.setCanvas() -- draw to screen again
         love.graphics.draw(buffers[buffers.current], 0, 0) -- draw 'ghosted' scene
@@ -37,10 +52,22 @@ function love.draw()
         
         love.graphics.setColor(r, g, b, a)
         -- draw new content over last frame
-        love.graphics.circle('fill', love.mouse.getX(), love.mouse.getY(), rd)
+        if currentMode == 0 then
+            love.graphics.circle('fill', love.mouse.getX(), love.mouse.getY(), rd)
+            love.graphics.setColor(1, 1, 1, 1)
+            love.graphics.circle('line', love.mouse.getX(), love.mouse.getY(), rd)
+        elseif currentMode == 1 then
+            love.graphics.rectangle('fill', love.mouse.getX() - rd / 2, love.mouse.getY() - rd / 2, rd, rd)
+            love.graphics.setColor(1, 1, 1, 1)
+            love.graphics.rectangle('line', love.mouse.getX() - rd / 2, love.mouse.getY() - rd / 2, rd, rd)
+        end
     end
     love.graphics.setColor(r, g, b, a)
     love.graphics.rectangle('fill', 0, 0, 100, 100)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setLineWidth(5)
+    love.graphics.rectangle('line', 0, 0, 100, 100)
+    love.graphics.setLineWidth(1)
 end
 function love.keypressed(k)
     if k == 'r' then
@@ -57,9 +84,21 @@ function love.mousepressed(x, y, button)
     end
 end
 function love.wheelmoved(x, dy)
-    if dy > 0 then
-        rd = rd + 10
-    elseif dy < 0 then
-        rd = rd - 10
+    if crtl then
+        if dy > 0 then
+            rd = rd + 10
+        elseif dy < 0 then
+            rd = rd - 10
+        end
+    else
+        if alt then
+            if dy > 0 or dy < 0 then
+                if currentMode == 0 then
+                    currentMode = 1
+                else
+                    currentMode = 0
+                end
+            end
+        end
     end
 end
